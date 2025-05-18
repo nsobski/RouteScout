9ce5984390471d5d9b6e20c32300c2667c8ec8ca
Nicole Sobski <ns103@wellesley.edu>
2025-05-18 15:26:49 -0400

    Initial commit

---
+++ RouteScout/RouteScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouteScoutApp.swift
+//  RouteScout
+//
+//  Created by Nicole Sobski on 5/18/25.
+//
+
+import SwiftUI
+
+@main
+struct RouteScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
